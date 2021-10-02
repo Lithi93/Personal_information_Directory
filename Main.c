0ab8002a78714148e93c606519a1878b6cd0a30b
Lithi93 <45199857+Lithi93@users.noreply.github.com>
2021-10-02 14:21:33 +0300

    Changes:
    added string hashing function since this makes the code a bit cleaner
    when using switch case statements.
    
    added core part of the code. Using the switch case statements to 
    determine
    what part of the code is executed. The statement is determined by
    the hash of the string that user gives as input.

---
+++ Main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+// remember that these are done with all lowcase keys
+#ifndef SYMBOL
+#define EXIT 2090237503
+#define HELP 2090324718
+#endif
+
+
+const unsigned long hash(const char *str) {
+        // returns strings hash, can be nicely used in switch case
+        unsigned long hash = 5381;
+        int c;
+
+        while ((c = *str++))
+                hash = ((hash << 5) + hash) + c;
+
+        return hash;
+}
+
+
+int main(){
+        // main action loop for user to do something
+        char user_input[20]; // stores user input
+
+        while (strcmp(user_input, "exit") != 0)  {
+                printf("\nWhat you want to do?\n");
+                printf("<<< ");
+                scanf("%s", &user_input);
+
+                printf("Users selection: %s and its hash %d\n", &user_input, hash(user_input));  // debug
+
+                switch (hash(user_input)) {
+                case EXIT:
+                        // exits programm
+                        printf("Exiting programm\n");
+                        break;
+
+                case HELP:
+                        // prints out all commands
+                        break;
+
+                default:
+                        // if no commands were correct
+                        printf("No such commands exists\n");
+
+
+                }
+
+
+        }
+
+        return 0;
+}
