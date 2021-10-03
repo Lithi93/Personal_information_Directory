218c5246c847ddab5913e0d47d5825f03cb70f25
Lithi93 <45199857+Lithi93@users.noreply.github.com>
2021-10-03 12:17:57 +0300

    Changes:
    - added function print commands.
    - changed scanf to fgets (for buffer overflow reasons)
    - added files list_of_commands.txt (available commands are housed here)
    and Personel_information_list.txt (information about the persons are stored here)

--- Main.c
+++ Main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
@@ -8,7 +9,6 @@
 #define EXIT 2090237503
 #define HELP 2090324718
 #endif
-
 
 const unsigned long hash(const char *str) {
         // returns strings hash, can be nicely used in switch case
@@ -21,23 +21,56 @@ const unsigned long hash(const char *str) {
         return hash;
 }
 
-
-char commands(){
+void print_commands(){
         //return all commands in the softaware
-        char command[20];
 
-        return command;
+        FILE *list_of_commands;
+        list_of_commands = fopen("list_of_commands.txt", "r");
+        char line[150];
+
+        // if file is not null read the file
+        printf("\n--------------------------------\n");
+        printf("COMMANDS:");
+        printf("\n--------------------------------\n");
+
+        // read file if it's nut NULL
+        if (list_of_commands != NULL) {
+                // read lines while the line is not NULL, if line becames NULL terminate loop.
+                while ( fgets(line, 30, list_of_commands) != NULL ) {
+                        printf("%s", line);
+                }
+
+        }else{
+                printf("File was NULL\n");
+        }
 }
 
+void add_person(/* arguments */) {
+        /* TODO code */
+}
 
-int main(){
+void remove_person(/* arguments */) {
+        /* TODO code */
+}
+
+void edit_person(/* arguments */) {
+        /* TODO code */
+}
+
+void main(){
         // main action loop for user to do something
         char user_input[20]; // stores user input
 
         while (strcmp(user_input, "exit") != 0)  {
-                printf("\nWhat you want to do?\n");
+
+                printf("\nWhat you want to do? (type help for commands)\n");
                 printf("<<< ");
-                scanf("%s", &user_input);
+                fgets(user_input, 20, stdin);
+
+                // removes newline from fgets. If the user names is longer than 0 and if the last chracter is \n
+                if ((strlen(user_input) > 0) && (user_input[strlen (user_input) - 1] == '\n')) {
+                        user_input[strlen (user_input) - 1] = '\0';  // the \n chatacter is replaced with null terminator
+                }
 
                 // make all characters in user_input to lowcase
                 for(int i = 0; user_input[i]; i++) {
@@ -48,17 +81,18 @@ int main(){
 
                 switch (hash(user_input)) {
                 case EXIT:
-                        // exits programm
-                        printf("Exiting programm\n");
+                        // exits program
+                        printf("Exiting program\n");
                         break;
 
                 case HELP:
                         // prints out all commands
+                        print_commands();
                         break;
 
                 default:
                         // if no commands were correct
-                        printf("No such commands exists\n");
+                        printf(">>> No such commands exists!!!\n");
 
 
                 }
@@ -66,5 +100,4 @@ int main(){
 
         }
 
-        return 0;
 }
